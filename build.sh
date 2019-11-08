100540ca05e3e044351bb0817cd8cdc0dde6f23e
Ezra Kissel <ezkissel@indiana.edu>
2019-11-08 10:30:10 -0500

    update to yarn build

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 
 export PREFIX=/opt
 
-
 echo "Setting up DLT-WEB..."
 cd dlt-web
-npm install
-sudo npm --log-level=error install -g pm2 bower
-bower install
+sudo npm install yarn -g
+yarn install
 cd -
-
